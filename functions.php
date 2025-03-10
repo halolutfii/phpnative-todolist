594c364a47ab9a1d3be3a8839f9fd37a402de452
Lutfi Cahya Nugraha <halolutfii@gmail.com>
2025-03-10 16:06:53 +0700

    first commit

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php 
+
+// Connection to DB
+function connectDB() {
+    $host = "localhost";
+    $user = "root";
+    $pass = "root";
+    $dbname = "my_database";
+
+    $conn = new mysqli($host, $user, $pass, $dbname);
+
+    if ($conn->connect_error) {
+        die("Koneksi gagal: " . $conn->connect_error);
+    }
+
+    return $conn;
+}
+
+
+?>
